409edc45d8b466f64d3c84fe5104ce510b2e909d
Marazzo <Joseph.Marazzo@tiaa.org>
2017-07-19 13:12:06 -0400

    this commit

--- output.java
+++ output.java
@@ -1,12 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.IOException;
 
-public class output {
+public class Cleaner {
 
-	private static final String FILENAME = "C:\\Users\\marazzj\\Desktop\\output.txt"; 
+	public static void readInput(String INPUTFILENAME) {
+		BufferedReader br = null;
+		FileReader fr = null;
 
-	public static void main(String[] args) {
+		try {
+
+			fr = new FileReader(INPUTFILENAME);
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+
+			br = new BufferedReader(new FileReader(INPUTFILENAME));
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+		}
+	}
+
+	public static void writeOutput(String FILENAME) {
 
 		BufferedWriter bw = null;
 		FileWriter fw = null;
@@ -38,11 +77,15 @@ public class output {
 			} catch (IOException ex) {
 
 				ex.printStackTrace();
-
 			}
 
 		}
 
 	}
 
+	public static void main(String[] args) {
+		readInput("C:\\airlines.txt");
+		writeOutput("C:\\Users\\marazzj\\Desktop\\output.txt");
+
+	}
 }
